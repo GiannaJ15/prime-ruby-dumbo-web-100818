a49e269b18714eb55b04076cd98dba484c94622e
gianna.jolibeth15 <gianna.jolibeth15@gmail.com>
2018-09-30 22:15:26 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,23 +1,17 @@
 # Add  code here!
 def prime?(int)
-array= (1..int.abs).to_a 
+if int == 0 || int <= 1 
+return false 
+end 
+if int ==2 
+return true 
+end 
+array= (2..int.abs).to_a 
  array.each do |x|
    int/(x)
-   if ((x> 1 && int% x == 0) || (int < 2))
+   if (x> 1 && int% x == 0)
      return false
    else return true 
    end 
  end
  end 
- 
- 
- def prime?(int)
-array= (2..int.abs).to_a  
- array.each do |x|
-   int.abs/(x)
-   if (int.abs% x == 0 && int != 2)
-    return false
-   end 
-end
-true
-end
