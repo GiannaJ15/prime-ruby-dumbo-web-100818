f47e940bfe4f9ad8a9f87e9843c5616c783183bd
gianna.jolibeth15 <gianna.jolibeth15@gmail.com>
2018-09-30 22:09:34 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,23 @@
 # Add  code here!
+def prime?(int)
+array= (1..int.abs).to_a 
+ array.each do |x|
+   int/(x)
+   if ((x> 1 && int% x == 0) || (int < 2))
+     return false
+   else return true 
+   end 
+ end
+ end 
+ 
+ 
+ def prime?(int)
+array= (2..int.abs).to_a  
+ array.each do |x|
+   int.abs/(x)
+   if (int.abs% x == 0 && int != 2)
+    return false
+   end 
+end
+true
+end
